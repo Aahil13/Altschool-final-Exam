b1905a58e20118ac2736e5dd766f62771647ebab
onyeanuna prince eze <onyeanunaprince@gmail.com>
2023-03-15 12:08:53 +0100

    Update: "Fix network bug 1.8"

--- script.js
+++ script.js
@@ -5,16 +5,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const Todo = require('./models/todo')
 
-mongoose.connect('mongodb://mongodb:27017/firstmongo', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  console.log("DB Connected!");
-});
+mongoose.connect('mongodb://mongodb:27017/firstmongo')
 
 app.use('/', express.static(path.resolve(__dirname, 'assets')))
 
