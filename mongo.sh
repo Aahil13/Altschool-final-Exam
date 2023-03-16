60f1f0277ce366b2919e26efa6e2590d1f897611
onyeanuna prince eze <onyeanunaprince@gmail.com>
2023-03-16 02:33:56 +0100

    Update: "Fix network bug 2.8"

--- mongo.sh
+++ mongo.sh
@@ -6,6 +6,8 @@ wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | apt-key add -
 # Install gnupg if it is not already installed
 apt-get install -y gnupg
 
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+
 # Add the MongoDB repository to the list of sources
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
@@ -16,7 +18,7 @@ apt-get update
 apt-get install -y mongodb-org
 
 # Start MongoDB
-service start mongod
+systemctl start mongod
 
 # Enable MongoDB to start on boot
-service enable mongod
+systemctl enable mongod
